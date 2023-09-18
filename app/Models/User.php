78e7728333b102e3001cc482750510305290d4ce
Dipo Oktama <dipooktama@usu.ac.id>
2023-09-18 13:40:24 +0700

    modded the model so that now it extends Authenticatable

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
     //use HasFactory;
     protected $table = 'user';
@@ -18,5 +19,9 @@ class User extends Model
         'name',
         'position',
         'role',
+    ];
+
+    protected $hidden = [
+        'password',
     ];
 }
